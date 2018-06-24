f6d137f202b0094a3d7abf21793009f66ee659b3
raulgonzalezcz <raul.gonzalezcz@udlap.mx>
2018-06-24 00:00:54 -0500

    Simple websocket conenction - Configuration

---
+++ README.md
@@ -0,0 +1,21 @@
+# Example of Node + Express + Typescript to develop a simple websocket
+Examples of projects in order to use more tools for web development.
+
+## Description
+I watch videos and search projects in which it is applied new tools. Steps:
+1. Type ./node_modules/.bin/tsc or simply tsc (if typescript is installed globally)
+2. node ./dist/server/server.js
+3. Add the server address ws://localhost:8999, press connect and send a message.
+
+
+## Special Thanks
+This material was provided mainly by https://medium.com/factory-mind/websocket-node-js-express-step-by-step-using-typescript-725114ad5fe4, another reference is http://mherman.org/blog/2016/11/05/developing-a-restful-api-with-node-and-typescript/
+
+## Programming language
+```[typescript]
+# Typescript
+```
+
+## Author
+**Raúl González Cruz**
+>*Computer Systems Engineering, UDLAP*
---
+++ dist/server/server.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const app = express();
+//initialize a simple http server
+const server = http.createServer(app);
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+wss.on('connection', (ws) => {
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+    let addr = server.address();
+    let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+    console.log(`Server started on ${bind} :`);
+});
+//# sourceMappingURL=server.js.map
